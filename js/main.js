e08d649fb88a088e8c3bd4fa42464afd2b790120
Appliberated <develop@appliberated.com>
2017-08-17 20:01:29 +0300

    Add startup color; finish documenting TypeScript code

--- js/main.js
+++ js/main.js
@@ -1,53 +1,83 @@
-var SCFGen = (function () {
-    function SCFGen() {
+/**
+ * @license
+ * Copyright 2017 Appliberated
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/** The Simple Color File Generator class. */
+var SCFGenerator = (function () {
+    function SCFGenerator() {
     }
-    SCFGen.prototype.updateDownloadAnchor = function (anchor) {
+    SCFGenerator.prototype.updateDownloadAnchor = function (anchor) {
         // Replace the current color value in the HTML content template
-        var scfContents = SCFGen.fileContentsTemplate.replace(new RegExp(SCFGen.colorVar, "g"), this.color);
+        var scfContents = SCFGenerator.fileContentsTemplate.replace(new RegExp(SCFGenerator.colorVar, "g"), this.color);
         // Create the HTML content blob
         var scfBlob = new Blob([scfContents], { type: 'text/html' });
         // Update the anchor's href property to the URL of the blob
         anchor.href = URL.createObjectURL(scfBlob);
         // Replace the current color value in the file name template and update the anchor's download property
-        anchor.download = SCFGen.fileNameTemplate.replace(SCFGen.colorVar, this.color);
+        anchor.download = SCFGenerator.fileNameTemplate.replace(SCFGenerator.colorVar, this.color);
     };
     /** The color variable in the templates. */
-    SCFGen.colorVar = "{color}";
+    SCFGenerator.colorVar = "{color}";
     /** The default name template of the Simple Color File. */
-    SCFGen.fileNameTemplate = "color-" + SCFGen.colorVar + ".html";
+    SCFGenerator.fileNameTemplate = "color-" + SCFGenerator.colorVar + ".html";
     /** The default HTML content template of the Simple Color File. */
-    SCFGen.fileContentsTemplate = "<html>\n<head>\n    <title>Color " + SCFGen.colorVar + "</title>\n</head>\n<body style=\"background: " + SCFGen.colorVar + "\">\n</body>\n</html>";
-    return SCFGen;
+    SCFGenerator.fileContentsTemplate = "<html>\n<head>\n    <title>Color " + SCFGenerator.colorVar + "</title>\n</head>\n<body style=\"background: " + SCFGenerator.colorVar + "\">\n</body>\n</html>";
+    return SCFGenerator;
 }());
+/** The Simple Color File Application class. */
 var SCFApp = (function () {
     function SCFApp() {
         var _this = this;
+        /** Check for a valid color and update the color when the user changes the content of the Color input. */
         this.onColorInputInput = function (event) {
             document.body.style.backgroundColor = "";
             document.body.style.backgroundColor = _this.colorInput.value;
             if (document.body.style.backgroundColor != "") {
+                // The body has accepted the color input, we have a valid color, show the download anchor
                 _this.color = _this.colorInput.value;
                 _this.downloadAnchor.classList.remove("hidden");
             }
             else {
+                // Hide the download anchor if color input is invalid
                 _this.downloadAnchor.classList.add("hidden");
             }
         };
+        /**
+         * Generate the Simple Color File and update the download anchor when the user clicks it, before the browser
+         * starts the download.
+         */
         this.onDownloadAnchorClick = function (event) {
             _this.scfGen.color = _this.color;
             _this.scfGen.updateDownloadAnchor(_this.downloadAnchor);
             return true;
         };
     }
-    SCFApp.prototype.attachEvents = function () {
-        this.scfGen = new SCFGen();
+    /** Initialize the app and add the event handlers. */
+    SCFApp.prototype.init = function () {
+        // Create the SCF generator
+        this.scfGen = new SCFGenerator();
         this.colorInput = document.getElementById("color");
         this.colorInput.addEventListener("input", this.onColorInputInput);
         this.downloadAnchor = document.getElementById("download");
         this.downloadAnchor.addEventListener("click", this.onDownloadAnchorClick);
+        // Init startup color
+        this.onColorInputInput(null);
     };
     return SCFApp;
 }());
+// Initialize the application
 var scfApp = new SCFApp();
-scfApp.attachEvents();
+scfApp.init();
 //# sourceMappingURL=main.js.map
--- js/main.ts
+++ js/main.ts
@@ -1,18 +1,36 @@
-class SCFGen {
+/**
+ * @license
+ * Copyright 2017 Appliberated
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/** The Simple Color File Generator class. */
+class SCFGenerator {
 
     /** The color variable in the templates. */
     private static readonly colorVar: string = "{color}";
 
     /** The default name template of the Simple Color File. */
-    private static readonly fileNameTemplate: string = `color-${SCFGen.colorVar}.html`;
+    private static readonly fileNameTemplate: string = `color-${SCFGenerator.colorVar}.html`;
 
     /** The default HTML content template of the Simple Color File. */
     private static readonly fileContentsTemplate: string = 
 `<html>
 <head>
-    <title>Color ${SCFGen.colorVar}</title>
+    <title>Color ${SCFGenerator.colorVar}</title>
 </head>
-<body style="background: ${SCFGen.colorVar}">
+<body style="background: ${SCFGenerator.colorVar}">
 </body>
 </html>`;
 
@@ -22,7 +40,7 @@ class SCFGen {
     updateDownloadAnchor(anchor: HTMLAnchorElement) {
 
         // Replace the current color value in the HTML content template
-        let scfContents = SCFGen.fileContentsTemplate.replace(new RegExp(SCFGen.colorVar, "g"), this.color);
+        let scfContents = SCFGenerator.fileContentsTemplate.replace(new RegExp(SCFGenerator.colorVar, "g"), this.color);
 
         // Create the HTML content blob
         let scfBlob = new Blob([scfContents], {type : 'text/html'});
@@ -31,47 +49,66 @@ class SCFGen {
         anchor.href = URL.createObjectURL(scfBlob);
 
         // Replace the current color value in the file name template and update the anchor's download property
-        anchor.download = SCFGen.fileNameTemplate.replace(SCFGen.colorVar, this.color);
+        anchor.download = SCFGenerator.fileNameTemplate.replace(SCFGenerator.colorVar, this.color);
     }
 }
 
-
+/** The Simple Color File Application class. */
 class SCFApp {
 
+    /** The current color value to be exported to the Simple Color File. */
     private color: string;
-    private scfGen: SCFGen;
+
+    /** A SCF generator instance. */
+    private scfGen: SCFGenerator;
+
+    /** The color HTML input element. */
     private colorInput : HTMLInputElement;
+
+    /** The download HTML anchor element. */
     private downloadAnchor : HTMLAnchorElement;
 
-
+    /** Check for a valid color and update the color when the user changes the content of the Color input. */
     private onColorInputInput = (event) => {
         document.body.style.backgroundColor = "";
         document.body.style.backgroundColor = this.colorInput.value;
 
         if (document.body.style.backgroundColor != "") {
+            // The body has accepted the color input, we have a valid color, show the download anchor
             this.color = this.colorInput.value;
             this.downloadAnchor.classList.remove("hidden");
         } else {
+            // Hide the download anchor if color input is invalid
             this.downloadAnchor.classList.add("hidden");
         }
     }
 
+    /** 
+     * Generate the Simple Color File and update the download anchor when the user clicks it, before the browser
+     * starts the download.
+     */
     private onDownloadAnchorClick = (event) => {
         this.scfGen.color = this.color;
         this.scfGen.updateDownloadAnchor(this.downloadAnchor);
         return true;
     }
 
-    attachEvents() {
-        this.scfGen = new SCFGen();
+    /** Initialize the app and add the event handlers. */
+    init() {
+        // Create the SCF generator
+        this.scfGen = new SCFGenerator();
 
         this.colorInput = <HTMLInputElement>document.getElementById("color");
         this.colorInput.addEventListener("input", this.onColorInputInput);
 
         this.downloadAnchor = <HTMLAnchorElement>document.getElementById("download");
         this.downloadAnchor.addEventListener("click", this.onDownloadAnchorClick);
+
+        // Init startup color
+        this.onColorInputInput(null);
     }
 }
 
+// Initialize the application
 let scfApp = new SCFApp();
-scfApp.attachEvents();
+scfApp.init();
